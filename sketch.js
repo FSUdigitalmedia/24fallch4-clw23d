ea049ab5016bafce28bf5edd1c638ba9ec3db7bc
Cassidy Williams <clw23d@fsu.edu>
2024-09-16 09:23:58 -0400

    attempt to make text run from mouse

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER)
 }
 
+let xpos = 200
+let ypos = 200
+
 function draw() {
+  textSize(22);
+  fill('red');
   background(125);
-  circle(width/2, height/2, 40);
+  //circle(width/2, height/2, 40);
+  text('ESCAPE', xpos, ypos);
+}
+
+if (mouseX = xpos) {
+  let xpos = mouseX + 10
+} if (mousey = ypos) {
+  let ypos = mouseY
 }
